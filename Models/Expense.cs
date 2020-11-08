e3f85fbcab0cc320d8d3cf271759479065f934a4
iulian agafitei <agafitei.iulian.nicolae@gmail.com>
2020-11-08 16:32:05 +0200

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agafitei_Iulian_Lab5.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public string Secret { get; set; }
+
+    }
+}
